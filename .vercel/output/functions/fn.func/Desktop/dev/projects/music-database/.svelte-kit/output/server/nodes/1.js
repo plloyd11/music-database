16537c24372f5312e5524b17bef4f0991e744b9e
Pete Lloyd <plloyd11@gmail.com>
2023-10-24 12:17:30 -0400

    fix 500 error

--- .vercel/output/functions/fn.func/Desktop/dev/projects/music-database/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/Desktop/dev/projects/music-database/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.83c1245b.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.fb9b0121.js","_app/immutable/chunks/singletons.3a547e5e.js"];
+export const imports = ["_app/immutable/nodes/1.922b252e.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.fb9b0121.js","_app/immutable/chunks/singletons.e85888d9.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/Desktop/dev/projects/music-database/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/Desktop/dev/projects/music-database/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.d481bab5.js","app":"_app/immutable/entry/app.042fcdd5.js","imports":["_app/immutable/entry/start.d481bab5.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/singletons.3a547e5e.js","_app/immutable/entry/app.042fcdd5.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.fb9b0121.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.500ed7fa.js","app":"_app/immutable/entry/app.92d35e7c.js","imports":["_app/immutable/entry/start.500ed7fa.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/singletons.e85888d9.js","_app/immutable/entry/app.92d35e7c.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.fb9b0121.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
